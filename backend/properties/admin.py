6c9daf84385d54ef3a0e8a785352ec0956abc2be
Szymon Jędrzychowski <Szymon54322@gmail.com>
2025-06-24 19:06:40 +0200

    Added models to admin site

--- backend/properties/admin.py
+++ backend/properties/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Payment, Property, Lease
 
 # Register your models here.
+
+admin.site.register(Payment)
+admin.site.register(Property)
+admin.site.register(Lease)
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('role', 'phone_number')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('role', 'phone_number')}),
+    )
