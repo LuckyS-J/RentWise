0ccb57a88e8bf7213d7fc5a3cfc1ec5facb0392e
Szymon Jędrzychowski <Szymon54322@gmail.com>
2025-06-26 21:05:26 +0200

    Added CRUD API to properties

--- backend/properties/views.py
+++ backend/properties/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Payment, Property, Lease
@@ -14,3 +14,36 @@ class PropertyViewSet(APIView):
     properties = Property.objects.filter(owner=request.user)
     serializer = PropertySerializer(properties, many=True)
     return Response(serializer.data , status=200)
+  
+  def post(self, request):
+    serializer = PropertySerializer(data=request.data)
+    if serializer.is_valid():
+      serializer.save(owner=request.user)
+      return Response(serializer.data, status=201)
+    else:
+      return Response(serializer.errors, status=400)
+  
+
+class PropertyDetailView(APIView):
+
+  permission_classes = [IsAuthenticated]
+
+  def get(self, request, id):
+    property = get_object_or_404(Property, owner=request.user, id=id)
+    serializer = PropertySerializer(property)
+    return Response(serializer.data, status=200)
+    
+  def put(self, request, id):
+    property = get_object_or_404(Property, owner=request.user, id=id)
+    serializer = PropertySerializer(instance=property, data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status=201)
+    else:
+      return Response(serializer.errors, status=400)
+    
+  def delete(self, request, id):
+    property = get_object_or_404(Property, owner=request.user, id=id)
+    property.delete()
+    return Response(status=204)
+  
